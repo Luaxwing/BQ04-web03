3f767e5bccc0b70cbe7c04ad94dc22d2945a4fe4
Luaxwing <nick890111@gmail.com>
2024-01-22 10:26:02 +0800

    20240122:建立共同函式庫

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include_once "./api/db.php" ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0039) -->
 <html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
@@ -33,6 +35,15 @@
             </span>
                     </div>
         <div id="right">
+            <?php
+$do=$_GET['do']??'admin';
+$file="./front/($do).php";
+if(file_exists($file)){
+    include $file;
+}else{
+    include "./front/admin.php";
+}
+            ?>
         	        </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
         	頁尾版權 :        </div>
