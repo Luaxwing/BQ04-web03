6f08b961cae01c3b7e97cbb53b180f65797a010b
Luaxwing <nick890111@gmail.com>
2024-01-31 11:09:40 +0800

    20240131:script api會員編輯功能

--- api/reg.php
+++ api/reg.php
@@ -1,4 +1,20 @@
 <?php include_once 'db.php';
 
-$_POST['regdate']=date("Y-m-d");
+// $_POST['regdate']=date("Y-m-d");
+// $Mem->save($_POST);
+// 原code-(見save_mem.php)
+
+
+
+
+if(!isset($_POST['id'])){
+    // 註冊日期++(reg.php)
+    $_POST['regdate']=date("Y-m-d");
+}
+
+// 新增or編輯(兩者該行皆相同，因此合併)
 $Mem->save($_POST);
+
+if(isset($_POST['id'])){
+    to("../back.php?do=mem");
+}
---
+++ api/save_mem.php
@@ -0,0 +1,8 @@
+<?php
+;
+/* include_once 'db.php';
+
+$Mem->save($_POST);
+to("../back.php?do=mem");*/
+
+//合併至reg.php
