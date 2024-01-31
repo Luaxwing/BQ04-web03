d73d5afbdce34c2ed8052eaf9c1e8c63981afc0e
Luaxwing <nick890111@gmail.com>
2024-01-31 08:35:34 +0800

    20240131:會員|管理 登出

--- index.php
+++ index.php
@@ -26,13 +26,38 @@
                 <a href="?do=news">最新消息</a> |
                 <a href="?do=look">購物流程</a> |
                 <a href="?do=buycart">購物車</a> |
-                <a href="?do=login">會員登入</a> |
-                <a href="?do=admin">管理登入</a>
+                <?php
+                if (isset($_SESSION['mem'])) {
+
+                    ?>
+
+
+                    <a href="./api/logout.php">會員登出</a> |
+
+                    <?php
+                } else {
+                    echo "<a href='?do=login'>會員登入</a> |";
+                }
+                ?>
+
+                <?php
+                if (isset($_SESSION['admin'])) {
+                    ?>
+                    <a href="./back.php">返回管理</a>
+
+                    <?php
+                } else {
+                    ?>
+                    <a href="?do=admin">管理登入</a>
+
+                    <?php
+                }
+                ?>
             </div>
             <marquee behavior="" direction="">
-            年終特賣會開跑了 &nbsp;   情人節特惠活動 &nbsp; 
+                年終特賣會開跑了 &nbsp; 情人節特惠活動 &nbsp;
             </marquee>
-            
+
         </div>
         <div id="left" class="ct">
             <div style="min-height:400px;">
@@ -44,7 +69,7 @@
             </span>
         </div>
         <div id="right">
-            
+
             <?php
             $do = $_GET['do'] ?? 'main';
             $file = "./front/{$do}.php";
@@ -57,7 +82,7 @@
         </div>
         <div id="bottom" style="line-height:70px;background:url(icon/bot.png); color:#FFF;" class="ct">
             <!-- 頁尾版權 :  -->
-        <?=$Bottom->find(["sh"=>1])['bottom'];?>
+            <?= $Bottom->find(["sh" => 1])['bottom']; ?>
         </div>
     </div>
 
