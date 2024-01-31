a4988d90ef48af95374881dfce2ce56d8844e302
Luaxwing <nick890111@gmail.com>
2024-01-31 13:59:14 +0800

    20240131: api//save_type.php ||調整

--- api/save_type.php
+++ api/save_type.php
@@ -1,11 +1,12 @@
 <?php
 // init
 include_once "db.php";
-$DB=new DB('type');
+// $DB=new DB('type');
 
 // start
 
 // $_POST=['name'=name , 'big_id'=big_id] //Note
-$DB->save($_POST);
+// $DB->save($_POST);
+$Type->save($_POST);
 
 // end
