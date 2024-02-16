13cb05fe32f64f37cb129339206e1d6a8c546456
Luaxwing <nick890111@gmail.com>
2024-02-16 13:17:35 +0800

    20240216:index.php //將商品名稱_圖片_價錢_簡介呈現於畫面上

--- front/main.php
+++ front/main.php
@@ -24,7 +24,25 @@ if($type==0){
 
 <?php
 foreach($goods as $good){
-    echo $good['name']."<br>";
+    // echo $good['name']."<br>";
+    ?>
+    
+
+    <div class="item">
+        <div class="img">
+<img src="./img/<?=$good['img']?>" style="width:200px;height:150px">
+        </div>
+        <div class="info">
+            <div class="ct tt"><?=$good['name']?></div>
+            <div>價錢:<?=$good['price']?></div>
+            <div>規格:<?=$good['spec']?></div>
+            <div>簡介:<?=$good['intro']?></div>
+        </div>
+    </div>
+    
+    
+    
+    <?php
 }
 ?>
 <!-- <button onclick="location.href='?do=buycart'">我要購買</button> -->
